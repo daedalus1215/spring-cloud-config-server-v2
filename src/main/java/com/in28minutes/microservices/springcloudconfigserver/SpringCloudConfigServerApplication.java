1e6b3c07116c23731a07b709ad4fb674c2331fe0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-26 11:27:03 -0400

    setup properties

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
